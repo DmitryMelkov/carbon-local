d9a49bb18cb676edc9b42c6309758934f1e16354
Dmitry <melkov.dmitriy40@gmail.com>
2024-11-01 13:59:30 +0500

    add mnemo and current

--- js/app.js
+++ js/app.js
@@ -1,18 +1,51 @@
-  // Функция для переключения вкладок
-  function openTab(tabId) {
-    // Скрываем все табы
-    const tabs = document.querySelectorAll('.tab-content');
-    tabs.forEach(tab => {
-        tab.classList.remove('active');
-    });
+document.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tab-container__button');
+  const subTabs = document.querySelectorAll('.sub-tab-button');
 
-    // Убираем активный класс с кнопок
-    const buttons = document.querySelectorAll('.tab-button');
-    buttons.forEach(button => {
-        button.classList.remove('active');
+  const adjustIframeHeight = () => {
+    const iframes = document.querySelectorAll('.iframe-container iframe');
+    const height = window.innerHeight - 150;
+    iframes.forEach((iframe) => {
+      iframe.style.height = `${height}px`;
     });
+  }
 
-    // Показываем выбранную вкладку и добавляем активный класс к кнопке
-    document.getElementById(tabId).classList.add('active');
-    event.currentTarget.classList.add('active');
-}
+  adjustIframeHeight();
+  window.addEventListener('resize', adjustIframeHeight);
+
+  // Основные вкладки
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+      document.querySelectorAll('.tab-content').forEach((content) => content.classList.remove('tab-content--active'));
+      tabs.forEach((button) => button.classList.remove('tab-container__button--active'));
+
+      const tabId = tab.getAttribute('data-tab');
+      document.getElementById(tabId).classList.add('tab-content--active');
+      tab.classList.add('tab-container__button--active');
+
+      // Устанавливаем начальный URL во фрейм при переключении основной вкладки
+      const iframe = document.querySelector(`#${tabId} iframe`);
+      const defaultUrl = tabId === 'carbonization-1' ? '/mnemo-pech-vr-1' : '/mnemo-pech-vr-2';
+      iframe.src = `http://169.254.0.156:3002${defaultUrl}`;
+
+      // Обновляем состояние активной под-вкладки
+      const activeTab = document.getElementById(tabId);
+      activeTab.querySelectorAll('.sub-tab-button').forEach((btn) => btn.classList.remove('sub-tab-button--active'));
+      activeTab.querySelector(`.sub-tab-button[data-url="${defaultUrl}"]`).classList.add('sub-tab-button--active');
+    });
+  });
+
+  // Под-вкладки
+  subTabs.forEach((button) => {
+    button.addEventListener('click', () => {
+      const activeTab = document.querySelector('.tab-content.tab-content--active');
+      const iframe = activeTab.querySelector('iframe');
+      const url = button.getAttribute('data-url');
+
+      iframe.src = `http://169.254.0.156:3002${url}`;
+
+      activeTab.querySelectorAll('.sub-tab-button').forEach((btn) => btn.classList.remove('sub-tab-button--active'));
+      button.classList.add('sub-tab-button--active');
+    });
+  });
+});
